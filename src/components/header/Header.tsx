d949d14284bcc3657a4a65481eddd6cbaad8a1ed
iLinn <11552398+iLinn@users.noreply.github.com>
2025-02-03 18:03:08 +0200

    Project setup

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,22 @@
+import reactLogo from '/src/assets/react.svg';
+import './Header.css';
+
+function Header() {
+  return (
+    <header>
+      <a href="https://react.dev" target="_blank" className="logo-link">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+        <span>React</span>
+      </a> 
+      <nav>
+        <ul className="nav-list">
+          <li>Career</li>
+          <li>About</li>
+          <li>Contacts</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Header from './components/header/Header';
+import MainContent from './components/main-component/Main';
+import Footer from './components/footer/Footer';
+// import App from './components/app/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Header />
+    <MainContent />
+    <Footer />
   </StrictMode>,
 )
