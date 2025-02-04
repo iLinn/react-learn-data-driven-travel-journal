f9863437c953488c307ccb0811a7dbb67c5fa30e
iLinn <11552398+iLinn@users.noreply.github.com>
2025-02-04 18:40:14 +0200

    Adjusted header styles; Added common css classes;

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,13 +8,13 @@ function Header() {
       <div>
         <a href="https://react.dev" target="_blank" className="logo-link flex-grow-1">
           <img src={reactLogo} className="shadow react-logo" alt="React logo" />
-          <span>React</span>
+          <span className="fw-bolder">React</span>
         </a>
       </div>
       <div>
         <a href="" target="_blank" className="icon-link shadow flex-grow-1">
           <img src={globeLogo} className="icon globe" alt="globe logo" />
-          <span>my travel journal</span>
+          <span className='fw-bolder'>my travel journal.</span>
         </a>
       </div>
       <span className="flex-basis-inherit"></span>
