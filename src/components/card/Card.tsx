1f37c30e3e5daf3fb748ce2cf55d2cc28f6f4135
iLinn <11552398+iLinn@users.noreply.github.com>
2025-02-04 20:13:07 +0200

    Updated CardProps

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,21 +1,23 @@
 import './Card.css';
 
 interface CardProps {
-  imgSrc: string;
-  imgAlt: string;
+  img: {
+    src: string;
+    alt: string;
+  };
   place: string;
   mapLink: string;
   title: string;
-  date: string;
+  dates: string;
   description: string;
 };
 
 export default function Card(props: CardProps) {
-  const { imgSrc, imgAlt, place, mapLink, title, date, description } = props;
+  const { img, place, mapLink, title, dates, description } = props;
   return (
     <article className="card flex-row gap-1 mb-1">
       <div className="card-image-container">
-        <img className="card-image" src={imgSrc} alt={imgAlt} />
+        <img className="card-image" src={img?.src} alt={img?.alt} />
       </div>
       <div className="card-content">
         <span className="marker-icon place">{place}</span>
@@ -23,9 +25,9 @@ export default function Card(props: CardProps) {
             target="_blank"
             href={mapLink}>View on Google Maps</a>
 
-        <h2>{title}</h2>
-        <h4>{date}</h4>
-        <p>{description}</p>
+        {title ? <h2>{title}</h2> : <h2>Special Place</h2>}
+        <h4>{dates ? dates : "a while ago"}</h4>
+        {description && <p>{description}</p>}
       </div>
     </article>
   );
